6e00c7e722e762dbe4a346dd7b7e5e38b4cca2c7
LIANG JINXIN <JINXIN003@m.scnu.edu.cn>
2025-09-29 23:25:30 +0800

    [feat] add net data and its operator

---
+++ src/nerual_net/net_data/net_data.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <string>
+#include <cstring>
+#include <vector>
+#include <iostream>
+namespace nn {
+struct NetData {
+    explicit NetData(size_t _size) : size_(_size), data_(std::malloc(_size)) {
+        std::cout << "create NetData, size is " << _size << std::endl;
+        if (!data_) {
+            throw std::bad_alloc();
+        }
+    }
+    ~NetData() {
+        releaseData();
+    }
+
+    NetData(const NetData &) = delete;
+    NetData &operator=(const NetData &) = delete;
+
+    NetData(NetData &&other) noexcept
+        : data_(other.data_), size_(other.size_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+        std::cout << "move NetData, size is " << size_ << std::endl;
+    }
+
+    NetData &operator=(NetData &&other) noexcept {
+        if (this != &other) {
+            releaseData();
+            data_ = other.data_;
+            size_ = other.size_;
+            other.data_ = nullptr;
+            other.size_ = 0;
+            std::cout << "operator= move NetData, size is " << size_ << std::endl;
+        }
+        return *this;
+    }
+
+    ///< 深拷贝
+    NetData copy() const {
+        NetData data(size_);
+        std::memcpy(data.data_, data_, size_);
+        std::cout << "copy NetData, size is " << size_ << std::endl;
+        return data;
+    }
+
+    const size_t getSize() const noexcept { return size_; };
+    void        *getData() const noexcept { return data_; };
+
+    ///< 释放data
+    void releaseData() noexcept {
+        if (data_) {
+            std::cout << "release NetData, size is " << size_ << std::endl;
+            std::free(data_);
+            data_ = nullptr;
+            size_ = 0;
+        }
+    }
+
+private:
+    size_t size_{0};
+    void  *data_{nullptr};
+};
+}  // namespace nn
---
+++ src/nerual_net/net_data_op/net_data_op.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "nn_config.h"
+#include "net_data.h"
+
+namespace nn {
+class NetDataOp {
+public:
+    NetDataOp() = delete;
+    ~NetDataOp() = delete;
+    NetDataOp(const NetDataOp &) = delete;
+    NetDataOp &operator=(const NetDataOp &) = delete;
+    NetDataOp(NetDataOp &&) = delete;
+    NetDataOp &operator=(NetDataOp &&) = delete;
+
+    template < typename... Args >
+    static NetData concat(Args &&...args) {  ///< NetData拼接,支持多个NetData实例
+        static_assert((std::is_same_v< std::decay_t< Args >, NetData > && ...),
+                      "All arguments to concat must be of type NetData");
+        // 计算总大小
+        size_t total_size = (args.getSize() + ...);
+
+        NetData result(total_size);
+        size_t  offset = 0;
+
+        // 把每个 NetData 拷贝到 result 内存
+        static auto copy_fn = [&](NetData &nd) {
+            std::memcpy(static_cast< char * >(result.getData()) + offset, nd.getData(), nd.getSize());
+            offset += nd.getSize();
+        };
+
+        (copy_fn(args), ...);  // 展开参数包
+
+        return result;
+    }
+
+    template < typename T >
+    static void hwc2chw(_IN_OUT nn::NetData &_data, _IN size_t _channels, _IN size_t _height, _IN size_t _width) {  ///< 通道转换，T代表_data中的数据类型
+        std::vector< T > buffer(_data.getSize() / sizeof(T));
+        auto             src = reinterpret_cast< T * >(_data.getData());
+
+        for (int c = 0; c < _channels; ++c) {
+            for (int h = 0; h < _height; ++h) {
+                for (int w = 0; w < _width; ++w) {
+                    buffer[c * _height * _width + h * _width + w] =
+                        src[h * _width * _channels + w * _channels + c];
+                }
+            }
+        }
+        std::memcpy(_data.getData(), buffer.data(), _data.getSize());
+    }
+
+    template < typename T >
+    static void chw2hwc(_IN_OUT nn::NetData &_data, _IN size_t _channels, _IN size_t _height, _IN size_t _width) {  ///< 通道转换，T代表_data中的数据类型
+        std::vector< T > buffer(_data.getSize() / sizeof(T));
+        auto             src = reinterpret_cast< T * >(_data.getData());
+
+        for (size_t c = 0; c < _channels; ++c) {
+            for (size_t h = 0; h < _height; ++h) {
+                for (size_t w = 0; w < _width; ++w) {
+                    buffer[h * _width * _channels + w * _channels + c] =
+                        src[c * _height * _width + h * _width + w];
+                }
+            }
+        }
+        std::memcpy(_data.getData(), buffer.data(), _data.getSize());
+    }
+};
+
+}  // namespace nn
