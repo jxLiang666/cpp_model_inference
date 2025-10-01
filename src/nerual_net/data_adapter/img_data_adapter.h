029a9ab1a20711ff02890ccd7bc74442dea153af
LIANG JINXIN <JINXIN003@m.scnu.edu.cn>
2025-10-02 01:08:48 +0800

    [refactor] add the interface to implement code in img_data_adapter

--- src/nerual_net/data_adapter/img_data_adapter.h
+++ src/nerual_net/data_adapter/img_data_adapter.h
@@ -5,7 +5,9 @@ namespace nn {
 class ImgDataAdapter : public DataAdapterBase {
 protected:
     explicit ImgDataAdapter(cv::Size &_ori_size, cv::Size &_prep_size) : ori_size_(_ori_size), prep_size_(_prep_size) {};
-    cv::Size ori_size_;
-    cv::Size prep_size_;
+    virtual std::vector< std::vector< NetData > > doCreateInput(_IN const std::any &_input) = 0;
+    virtual std::any                              doCreateOutput(_OUT std::vector< std::vector< NetData > > &_output) = 0;
+    cv::Size                                      ori_size_;
+    cv::Size                                      prep_size_;
 };
 }  // namespace nn
